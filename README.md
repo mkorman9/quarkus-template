5890df8591dc3e74a8e4d09009d33126d5f2b2e3
Michal Korman <michal.korman@icloud.com>
2023-10-27 11:34:30 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Quarkus project template
+
+```sh
+git clone git@github.com:mkorman9/quarkus-template.git && rm -rf quarkus-template/.git quarkus-template/README.md
+```
---
+++ src/main/java/com/github/mkorman9/StubResource.java
@@ -0,0 +1,19 @@
+package com.github.mkorman9;
+
+import io.smallrye.common.annotation.RunOnVirtualThread;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes({})
+@RunOnVirtualThread
+public class StubResource {
+    @GET
+    public void getStub() {
+        // implement
+    }
+}
