975a68be61f3963be736227fd488f94370fe3413
Michal Korman <michal.korman@icloud.com>
2023-12-27 18:39:02 +0100

    don't default to virtual threads

--- src/main/kotlin/com/github/mkorman9/StubResource.kt
+++ src/main/kotlin/com/github/mkorman9/StubResource.kt
@@ -1,6 +1,5 @@
 package com.github.mkorman9
 
-import io.smallrye.common.annotation.RunOnVirtualThread
 import jakarta.ws.rs.Consumes
 import jakarta.ws.rs.GET
 import jakarta.ws.rs.Path
@@ -10,7 +9,6 @@ import jakarta.ws.rs.core.MediaType
 @Path("/")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(value = [])
-@RunOnVirtualThread
 class StubResource {
     @GET
     fun getStub() = "stub"
